8a79ac0428fea9c7da1585f10438f384667462f5
Sikriti Dakua <destrodev123@gmail.com>
2023-11-22 01:35:14 +0530

    feat: export types

---
+++ .changeset/rich-cows-glow.md
@@ -0,0 +1,5 @@
+---
+'sonner-svelte': patch
+---
+
+feat: export neccesary types
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
+export type { ToastType, ToastPosition } from './internal/types.js';
 export { toast } from './internal/toast.js';
 export { default as Toaster } from './components/toaster.svelte';
